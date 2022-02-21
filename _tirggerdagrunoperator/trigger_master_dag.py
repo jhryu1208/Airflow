041b9806b5f9eda3bfb4e2a25886273621ddd9c7
jhryu1208 <jhryu1208@gmail.com>
2022-02-22 00:12:10 +0900

    [edit] practice TriggerDagRunOperator

--- _tirggerdagrunoperator/trigger_master_dag.py
+++ _tirggerdagrunoperator/trigger_master_dag.py
@@ -1,1 +1,44 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
+
+default_args = {
+    'owner': 'zaid.ryu'
+}
+
+dag = DAG('Master_Dag_for_TDR',
+          description = 'Master Dag',
+          schedule_interval = '0 17 * * *',
+          default_args = default_args,
+          start_date = datetime(2022, 2, 19),
+          catchup = False)
+
+
+def trigger_func(context, obj):
+    obj.payload = {'name':'Zaid.Ryu',
+                   'talk':'this is my first code for TriggerDagRunOperator'}
+    return obj
+
+def print_ds(ds):
+    print(f'ds : {ds}')
+
+
+
+m_task1 = DummyOperator(task_id = 'master_task',
+                        dag = dag)
+
+m_task2 = TriggerDagRunOperator(task_id = 'trigger_slave',
+                                trigger_dag_id = 'Slave_Dag_for_TDR',
+                                python_callable = trigger_func,
+                                execution_date = "{{ ds }}",
+                                dag = dag)
+
+m_task3 = PythonOperator(task_id = 'printDS',
+                         python_callable = print_ds,
+                         op_args=["{{ ds }}"],
+                         dag = dag)
+
+
+m_task1 >> m_task2 >> m_task3
---
+++ _tirggerdagrunoperator/trigger_slave_dag.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    'owner': 'zaid.ryu'
+}
+
+dag = DAG('Slave_Dag_for_TDR',
+          description = 'Slave Dag',
+          schedule_interval = None,
+          default_args=default_args,
+          start_date = datetime(2022, 2, 19),
+          catchup = False)
+
+def pirnt_conf(**context):
+    dag_run = context['dag_run'].conf
+    print('{name} said \"{talk}\"'.format(**dag_run))
+
+def print_ds(ds):
+    print(f'ds : {ds}')
+
+
+
+s_task1 = DummyOperator(task_id = 'slave_task',
+                        dag = dag)
+
+s_task2 = PythonOperator(task_id = 'printDagRun',
+                         python_callable=pirnt_conf,
+                         provide_context=True,
+                         dag = dag)
+
+s_task3 = PythonOperator(task_id = 'printDS',
+                         python_callable = print_ds,
+                         op_args=["{{ ds }}"],
+                         dag = dag)
+
+s_task1 >> s_task2 >> s_task3
